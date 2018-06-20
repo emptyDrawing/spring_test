1152e6ac1122b9605aff7d641c373f8175b89699
amor0617 <amor061750@gmail.com>
2018-06-20 12:46:40 +0900

    프로젝트 이름변경중

--- springJava2/src/main/java/com/bit/spring04/springJava2/App.java
+++ springJava2/src/main/java/com/bit/spring04/springJava2/App.java
@@ -8,10 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	FrontModule fm = new FrontModule();
-    	fm.setModule(new Module01());
-    	
-    	fm.started();
     
     }
 }
