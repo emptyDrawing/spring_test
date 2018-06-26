e6a113f9a36f466a8700b8207a65945a886827f0
amor0617 <amor061750@gmail.com>
2018-06-26 17:23:47 +0900

    donelist : mybatis --> dao ver3

--- sts04/src/main/java/com/bit/myapp04/model/GuestDaoImf02.java
+++ sts04/src/main/java/com/bit/myapp04/model/GuestDaoImf02.java
@@ -4,44 +4,74 @@ import java.sql.SQLException;
 import java.util.List;
 
 import com.bit.myapp04.model.entity.GuestVo;
-import com.ibatis.sqlmap.client.SqlMapClient;
+//import com.ibatis.sqlmap.client.SqlMapClient;
 
 public class GuestDaoImf02 implements GuestDao{
 
-	/* 
-	 * 	using ibatis....
-	 * 
-	 * */
-
-	SqlMapClient sqlMapClient;
-	
-	public void setSqlMapClient(SqlMapClient sqlMapClient) {
-		this.sqlMapClient = sqlMapClient;
-	}
-	
 	@Override
 	public List<GuestVo> selectAll() throws SQLException {
-		return sqlMapClient.queryForList("selectAll");
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public GuestVo selectOne(int sabun) throws SQLException {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int insertOne(GuestVo bean) throws SQLException {
-		return sqlMapClient.update("insertOne",bean);
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	@Override
 	public int updateOne(GuestVo bean) throws SQLException {
-		return sqlMapClient.update("updateOne",bean);
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	@Override
 	public int deleteOne(int sabun) throws SQLException {
-		return sqlMapClient.update("deleteOne",sabun);
+		// TODO Auto-generated method stub
+		return 0;
 	}
+
+//	/* 
+//	 * 	using ibatis....
+//	 * 
+//	 * */
+//
+//	SqlMapClient sqlMapClient;
+//	
+//	public void setSqlMapClient(SqlMapClient sqlMapClient) {
+//		this.sqlMapClient = sqlMapClient;
+//	}
+//	
+//	@Override
+//	public List<GuestVo> selectAll() throws SQLException {
+//		return sqlMapClient.queryForList("selectAll");
+//	}
+//
+//	@Override
+//	public GuestVo selectOne(int sabun) throws SQLException {
+//		return null;
+//	}
+//
+//	@Override
+//	public int insertOne(GuestVo bean) throws SQLException {
+//		return sqlMapClient.update("insertOne",bean);
+//	}
+//
+//	@Override
+//	public int updateOne(GuestVo bean) throws SQLException {
+//		return sqlMapClient.update("updateOne",bean);
+//	}
+//
+//	@Override
+//	public int deleteOne(int sabun) throws SQLException {
+//		return sqlMapClient.update("deleteOne",sabun);
+//	}
 
 }
