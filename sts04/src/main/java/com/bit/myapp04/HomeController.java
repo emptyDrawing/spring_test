c8271f6848d39b67110083d5c5f8bedb03e17526
amor0617 <amor061750@gmail.com>
2018-06-26 13:17:51 +0900

    doneliist : bottstrap--> model // REST FULL - CRUD

--- sts04/src/main/java/com/bit/myapp04/HomeController.java
+++ sts04/src/main/java/com/bit/myapp04/HomeController.java
@@ -1,15 +1,22 @@
 package com.bit.myapp04;
 
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bit.myapp04.model.GuestDao;
+import com.bit.myapp04.model.entity.GuestVo;
 
 /**
  * Handles requests for the application home page.
@@ -19,21 +26,42 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	private GuestDao guestDao;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "home";
+	}
+	
+	@RequestMapping(value ="/guest/", method = RequestMethod.GET)
+	public String list(Model model) throws SQLException {
+		model.addAttribute("alist",guestDao.selectAll());
+		return "list";
+	}
+	
+	@RequestMapping(value="/guest/{sabun}", method= RequestMethod.GET)
+	public String detail(Model model, @PathVariable int sabun) throws SQLException {
+		model.addAttribute("sabun",sabun);
+		return list(model);
+	}
+
+	@RequestMapping(value ="/guest/", method = RequestMethod.POST)
+	public String insert(@ModelAttribute GuestVo bean) throws SQLException {
+		guestDao.insertOne(bean);
+		return "redirect:/guest/";
+	}
+	
+	@RequestMapping(value="/guest/{sabun}", method= RequestMethod.PUT)
+	public String edit(@PathVariable int sabun, @ModelAttribute GuestVo bean) throws SQLException {
+		guestDao.updateOne(bean);
+		return "redirect:/guest/"+sabun;
+	}
+
+	@RequestMapping(value ="/guest/{sabun}", method=RequestMethod.DELETE)
+	public String delete(@PathVariable int sabun) throws SQLException {
+		guestDao.deleteOne(sabun);
+		return "redirect:/guest/";
 	}
 	
 }
--- sts04/src/main/java/com/bit/myapp04/model/GueatDaoImf01.java
+++ sts04/src/main/java/com/bit/myapp04/model/GueatDaoImf01.java
@@ -1,9 +1,15 @@
 package com.bit.myapp04.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.bit.myapp04.model.entity.GuestVo;
 
@@ -11,40 +17,75 @@ public class GueatDaoImf01 implements GuestDao {
 
 	
 	JdbcTemplate jdbcTemplate;
-	
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
-	}
+	}// DI -- classpath:/applicationContext.xml
+
+	private RowMapper<GuestVo> rowMapper = new RowMapper<GuestVo>() {
+		@Override
+		public GuestVo mapRow(ResultSet rs, int rowNum) throws SQLException {
+			
+			return new GuestVo(
+						rs.getInt("sabun")	,rs.getString("name")	,rs.getDate("nalja")
+						,rs.getInt("pay")
+					);
+		}// mapRow() end
+	};// rowMapper end
 	
 	
 	@Override
 	public List<GuestVo> selectAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM GUEST ORDER BY SABUN";
+		return jdbcTemplate.query(sql, rowMapper );
 	}
 
 	@Override
 	public GuestVo selectOne(int sabun) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM GUEST where sabun = ?";
+		return jdbcTemplate.query(sql, new Object[] {sabun},rowMapper ).get(0);
 	}
 
 	@Override
-	public int insertOne(GuestVo bean) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int insertOne(final GuestVo bean) throws SQLException {
+		final String sql ="insert into guest values (?,?,sysdate,?)";
+		return jdbcTemplate.update( new PreparedStatementCreator() {
+
+			@Override
+			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, bean.getSabun());
+				pstmt.setString(2, bean.getName());
+				pstmt.setInt(3, bean.getPay());
+				return pstmt;
+			}});
 	}
 
 	@Override
-	public int updateOne(GuestVo bean) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int updateOne(final GuestVo bean) throws SQLException {
+		final String sql = "UPDATE GUEST SET NAME=?, PAY=? WHERE SABUN=?";
+		return jdbcTemplate.update(new PreparedStatementCreator() {
+
+			@Override
+			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, bean.getName());
+				pstmt.setInt(2, bean.getPay());
+				pstmt.setInt(3, bean.getSabun());
+				return pstmt;
+			}});
 	}
 
 	@Override
-	public int deleteOne(int sabun) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int deleteOne(final int sabun) throws SQLException {
+		final String sql ="DELETE FROM GUEST WHERE SABUN = ?";
+		return jdbcTemplate.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, sabun);
+				return pstmt;
+			}});
 	}
 
 }
